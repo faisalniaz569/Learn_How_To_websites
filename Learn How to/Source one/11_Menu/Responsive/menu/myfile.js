de72faf530667defbc954d7cb261e153352c906f
Faisal Niaz <73920072+faisalniaz569@users.noreply.github.com>
2025-05-10 21:15:53 +0600

    Update myfile.js

--- Learn How to/Source one/11_Menu/Responsive/menu/myfile.js
+++ Learn How to/Source one/11_Menu/Responsive/menu/myfile.js
@@ -1,34 +1,16 @@
 function myFunction() {
-    
-    "use strict";
-    
     var x = document.getElementById("myTopnav");
+    var barsIcon = x.querySelector(".fa-bars");
+    var timesIcon = x.querySelector(".fa-times");
+
     if (x.className === "topnav") {
         x.className += " responsive";
+        barsIcon.style.display = "none";
+        timesIcon.style.display = "inline";
     } else {
         x.className = "topnav";
+        barsIcon.style.display = "inline";
+        timesIcon.style.display = "none";
     }
-    
 }
 
-var $;
-
-$(function () {
-    
-    "use strict";
-    
-    $(".fa-bars").click(function () {
-        
-        $(".fa-bars").css("display", "none");
-        $(".fa-times").css("display", "block");
-        
-    });
-    
-    $(".fa-times").click(function () {
-        
-        $(".fa-times").css("display", "none");
-        $(".fa-bars").css("display", "block");
-        
-    });
-
-});
