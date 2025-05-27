862a3b51ce61ae7194e1eee62aa5e5afb703dbe3
Faisal Niaz <73920072+faisalniaz569@users.noreply.github.com>
2025-05-27 19:38:19 +0600

    Update myscript.js

--- Learn How to/Source one/14_Accordion/2_downup/myscript.js
+++ Learn How to/Source one/14_Accordion/2_downup/myscript.js
@@ -1,14 +1,19 @@
+/*global document*/
+
+function togglePanel() {
+    'use strict';
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+    } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+}
+
 var acc = document.getElementsByClassName("accordion");
 var i;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
+for (i = 0; i < acc.length; i += 1) {
+    acc[i].addEventListener("click", togglePanel);
 }
